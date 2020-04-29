5a6762d92039258d197eea0f85ecce34be8c04c0
Leonaldo <leonaldo.barbosa@gmail.com>
2020-04-29 19:33:39 -0300

    07. Returning Object References

--- gradebook/test/GradeBook.Tests/TypeTest.cs
+++ gradebook/test/GradeBook.Tests/TypeTest.cs
@@ -5,8 +5,20 @@ namespace GradeBook.Tests
 {
     public class TypeTest
     {
-         [Fact]
+        [Fact]
         public void Test1()
+        {
+            var book1 = GetBook("Book 1");
+            GetBookSetName(book1, "New Name"); 
+
+            Assert.Equal("Book 1", book1.Name);
+        }
+        private void GetBookSetName(Book book1, string name)
+        {
+            book1 = new Book(name);
+        }        
+         [Fact]
+        public void CanSetNameFromReference()
         {
             var book1 = GetBook("Book 1");
             SetName(book1, "New Name"); 
