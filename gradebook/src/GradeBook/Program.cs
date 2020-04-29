0a73e72dad0e457f1fbf3a4452aa421dfcf243ce
Leonaldo <leonaldo.barbosa@gmail.com>
2020-04-29 16:09:16 -0300

    08. Working with Static Members

--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -9,14 +9,18 @@ namespace GradeBook
         {
             var book = new Book("Scott's Grade Book");
             book.AddGrade(89.1);
+            book.AddGrade(90.5);
+            book.AddGrade(77.5);
+
 
             var grades = new List<double>(){12.7, 10.3, 6.11, 4.1};
             grades.Add(56.1);
             
             var result = 0.0;
-
+            var highGrade = double.MinValue;
             foreach(double number in grades)
             {
+                highGrade = Math.Max()
                 result += number;
             }
             result /= grades.Count;
