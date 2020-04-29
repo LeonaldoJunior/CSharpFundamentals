5b9bf3401e442d11fb26da8088ce4245eb537404
Leonaldo <leonaldo.barbosa@gmail.com>
2020-04-29 15:37:32 -0300

    05. Defining a Field

--- gradebook/src/GradeBook/Book.cs
+++ gradebook/src/GradeBook/Book.cs
@@ -1,10 +1,15 @@
+using System.Collections.Generic;
+
 namespace GradeBook
 {
     public class Book
     {
         public void AddGrade(double grade)
         {
-           
+            grades.Add(grade);
         }
+
+        List<double> grades;
+
     }
 }
