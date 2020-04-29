6694510b65814346c29090bf0f9294d0e82ce917
Leonaldo <leonaldo.barbosa@gmail.com>
2020-04-29 13:35:49 -0300

    07. Computing and Formatting the Result

--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -8,9 +8,7 @@ namespace GradeBook
         static void Main(string[]  args)
         {
             
-            var numbers = new[]{12.7, 10.3, 6.11, 4.1};
             var grades = new List<double>(){12.7, 10.3, 6.11, 4.1};
-            
             grades.Add(56.1);
             
             var result = 0.0;
@@ -19,8 +17,9 @@ namespace GradeBook
             {
                 result += number;
             }
+            result /= grades.Count;
 
-            Console.WriteLine(result);
+            Console.WriteLine($"The average grade is {result:N3}");
 
                         
             if(args.Length > 0)
