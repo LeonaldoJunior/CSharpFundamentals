7b9f7e9169c27f56e512374111ae95e12f5f2f67
Leonaldo <leonaldo.barbosa@gmail.com>
2020-04-29 16:14:58 -0300

    09. Computing Statistics

--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -18,13 +18,17 @@ namespace GradeBook
             
             var result = 0.0;
             var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
             foreach(double number in grades)
             {
-                highGrade = Math.Max()
+                highGrade = Math.Max(number, highGrade);
+                lowGrade = Math.Min(number, lowGrade);
+                
                 result += number;
             }
             result /= grades.Count;
-
+            System.Console.WriteLine($"The lowest grade is {lowGrade}");
+            System.Console.WriteLine($"The highest grade is {highGrade}");
             Console.WriteLine($"The average grade is {result:N3}");
 
         }
